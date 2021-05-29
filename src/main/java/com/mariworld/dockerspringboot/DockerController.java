447e57c64c847962e8f700e0230be3c30008cac3
MARI <mari20201101@gmail.com>
2021-05-30 01:22:28 +0900

    Create docker project

---
+++ src/main/java/com/mariworld/dockerspringboot/DockerController.java
@@ -0,0 +1,15 @@
+package com.mariworld.dockerspringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+
+    @GetMapping("/docker")
+    public String index(){
+        return "Hello docker";
+    }
+
+}
